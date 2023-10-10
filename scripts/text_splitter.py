3275f772a33317e801910e90bbc439f8e745ffcb
dimonalik <114773527+dimonalik@users.noreply.github.com>
2023-10-10 12:36:04 +0300

    Update text_splitter.py
    Translated from Ukrainian to international English

--- scripts/text_splitter.py
+++ scripts/text_splitter.py
@@ -2,37 +2,37 @@ import PySimpleGUI as sg
 splitted = ""
 text = ""
 sg.theme('DarkBlue4')
-layout = [[sg.Text('Сюди текст для розділення'), sg.InputText(size=(20, 5), key='input')],
-            [sg.Text('На скільки знаків ділимо?'),sg.InputText(size=(5, 1), key='symbols'),sg.Text('(Враховуйте додаткові символи на кількість постів)')],
-            [sg.Checkbox('Додавати кількість постів?', key='split')], 
-            [sg.Button('Розчленити')],
-            [sg.Multiline(splitted, key='-logtext-', size =(None, 10), rstrip=False)], [sg.Button('Очистити'), sg.Text('                                                © @uahave.fun', text_color='dark blue', font=('Helvetica', 10), justification='right')],[sg.Text('------------------------------------------------------')],[sg.Button('Закрити')]
+layout = [[sg.Text('Text for splitting'), sg.InputText(size=(20, 5), key='input')],
+            [sg.Text('How many symbols in 1 part?'),sg.InputText(size=(5, 1), key='symbols'),sg.Text('(Parts count uses some symbols)')],
+            [sg.Checkbox('Are we adding parts count?', key='split')], 
+            [sg.Button('Split')],
+            [sg.Multiline(splitted, key='-logtext-', size =(None, 10), rstrip=False)], [sg.Button('Clear'), sg.Text('                                                © @uahave.fun', text_color='dark blue', font=('Helvetica', 10), justification='right')],[sg.Text('------------------------------------------------------')],[sg.Button('Close')]
             ]
 
-window = sg.Window('Розділяч', layout)
+window = sg.Window('Splitter', layout)
 
 while True:
     event, values = window.read()
-    if event in (None, 'Закрити'):
+    if event in (None, 'Close'):
         break
-    if event == 'Розчленити':
+    if event == 'Split':
         if values['input'] == '':
-            sg.popup('Введіть текст!')
+            sg.popup('Enter text!')
             continue
         if values['symbols'].isdigit() == False:
-            sg.popup('Введіть кількість символів цифрами!')
+            sg.popup('Enter symbols amount with numeric!')
             continue
         if values['symbols'] == '':
-            sg.popup('Введіть кількість символів!')
+            sg.popup('Enter symbols amount!')
             continue
         if int(values['symbols']) < 1:
-            sg.popup('Введіть кількість символів більше 0!')
+            sg.popup('Enter symbols amount > 0!')
             continue
         if int(values['symbols']) > len(values['input']):
-            sg.popup('Введіть кількість символів менше довжини тексту!')
+            sg.popup('Symbols amount is larger than text length!')
             continue
         if int(values['symbols']) == len(values['input']):
-            sg.popup('Введіть кількість символів менше довжини тексту!')
+            sg.popup('Symbols amount is equal to text length!')
             continue
         es = [values['input'][i:i + int(values['symbols'])] for i in range(0, len(values['input']), int(values['symbols']))] 
         for i in es:
@@ -41,7 +41,7 @@ while True:
             else:
                 text = text + splitted + i + '\n' + '\n'
         window['-logtext-'].update(text)
-    if event == 'Очистити':
+    if event == 'Clear':
         window['-logtext-'].update('')
         text = ''
 window.close()
